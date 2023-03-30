2694c0b2011cd8f62780e04dd7abec9a497de081
teammate <timvarsey@gmail.com>
2023-03-30 12:01:22 +0300

    upgrade

--- dist/script.js
+++ dist/script.js
@@ -1,44 +1,44 @@
-// // задание 1
-// let a = prompt('Enter your name', 'My name is...')
-// alert('Nice to meet you ' + a)
-// // задание 2
-// let year = 2023
-// let birth = Number(prompt('Enter your year of birth'))
-// let age = year - birth
-// let age2 = age - 1
-// alert('You are ' + age + ' or ' + age2)
-// // задание 3
-// let b = Number(prompt('Введите сторону квадрата'))
-// let square = b * 4
-// alert('Периметр равен ' + square)
-// // задание 4
-// let radius = Number(prompt('Введите радиус окружности'))
-// let area = Math.PI * radius ** 2
-// alert('Площадь равна ' + area)
-// // задание 5
-// let km = Number(prompt('Введите расстояние в км'))
-// let time = Number(prompt('За какое время хотите доехать'))
-// let speed = km / time
-// alert('Вам нужно ехать со скоростью ' + speed + ' км/ч')
-// // задание 6
-// let dollar = Number(prompt('Введите количество долларов'))
-// let dollarRate = 0.92
-// let euro = dollar * dollarRate
-// alert('Вы имеете ' + euro + ' евро')
-// // задание 7
-// let storage = Number(prompt('Введите объём флешки'))
-// let files = Math.floor(storage / 0.82)
-// alert('На вашей флешке поместится ' + files + ' файлов')
-// // задание 8
-// let sum = Number(prompt('Сколько у вас денег?'))
-// let price = Number(prompt('Сколько стоит одна шоколадка?'))
-// let amount = Math.floor(sum / price)
-// let change = sum % price
-// alert('Вы можете купить ' + amount + ' шоколадок и у вас останется ' + change + ' рублей')
-// // задание 9
-// let digit = Number(prompt('Введите трехзначное число'))
-// let reverse = String(digit % 10) + String((Math.floor(digit / 10) % (Math.floor(digit / 100) * 10))) + String(Math.floor(digit / 100))
-// alert('Число ' + digit + ' наоборот будет ' + reverse)
-// // задание 10
-// let numb = Number(prompt('Введите число'))
-// numb % 2 ? alert('Число нечетное') : alert('Число четное')
+// задание 1
+var a = prompt('Enter your name', 'My name is...');
+alert('Nice to meet you ' + a);
+// задание 2
+var year = 2023;
+var birth = Number(prompt('Enter your year of birth'));
+var age = year - birth;
+var age2 = age - 1;
+alert('You are ' + age + ' or ' + age2);
+// задание 3
+var b = Number(prompt('Введите сторону квадрата'));
+var square = b * 4;
+alert('Периметр равен ' + square);
+// задание 4
+var radius = Number(prompt('Введите радиус окружности'));
+var area = Math.PI * Math.pow(radius, 2);
+alert('Площадь равна ' + area);
+// задание 5
+var km = Number(prompt('Введите расстояние в км'));
+var time = Number(prompt('За какое время хотите доехать'));
+var speed = km / time;
+alert('Вам нужно ехать со скоростью ' + speed + ' км/ч');
+// задание 6
+var dollar = Number(prompt('Введите количество долларов'));
+var dollarRate = 0.92;
+var euro = dollar * dollarRate;
+alert('Вы имеете ' + euro + ' евро');
+// задание 7
+var storage = Number(prompt('Введите объём флешки'));
+var files = Math.floor(storage / 0.82);
+alert('На вашей флешке поместится ' + files + ' файлов');
+// задание 8
+var sum = Number(prompt('Сколько у вас денег?'));
+var price = Number(prompt('Сколько стоит одна шоколадка?'));
+var amount = Math.floor(sum / price);
+var change = sum % price;
+alert('Вы можете купить ' + amount + ' шоколадок и у вас останется ' + change + ' рублей');
+// задание 9
+var digit = Number(prompt('Введите трехзначное число'));
+var reverse = String(digit % 10) + String((Math.floor(digit / 10) % (Math.floor(digit / 100) * 10))) + String(Math.floor(digit / 100));
+alert('Число ' + digit + ' наоборот будет ' + reverse);
+// задание 10
+var numb = Number(prompt('Введите число'));
+numb % 2 ? alert('Число нечетное') : alert('Число четное');
